3bf49e8dd0768a97b431e41515ba6f4bad14a06c
fajardedipratama <dedipratama135@gmail.com>
2021-03-20 10:32:18 +0700

    update

--- vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app/layouts/main.php
+++ vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app/layouts/main.php
@@ -33,6 +33,7 @@ if (Yii::$app->controller->action->id === 'login') {
         <meta charset="<?= Yii::$app->charset ?>"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <?= Html::csrfMetaTags() ?>
+        <link rel="shortcut icon" href="photos/favicon.ico?v=1" type="image/x-icon" />
         <title><?= Html::encode($this->title) ?></title>
         <?php $this->head() ?>
     </head>
