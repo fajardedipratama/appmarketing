eb9456d99854a820340d6c20ea12a110101ee68d
fajardedipratama <dedipratama135@gmail.com>
2022-02-09 16:48:12 +0700

    update

--- vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app/layouts/content.php
+++ vendor/dmstr/yii2-adminlte-asset/example-views/yiisoft/yii2-app/layouts/content.php
@@ -44,15 +44,14 @@ use dmstr\widgets\Alert;
 </footer>
 
 <!-- Control Sidebar -->
-<aside class="control-sidebar control-sidebar-dark">
-    <!-- Create the tabs -->
+<!-- <aside class="control-sidebar control-sidebar-dark">
+
     <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
         <li><a href="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-home"></i></a></li>
         <li><a href="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
-    </ul>
+    </ul> -->
     <!-- Tab panes -->
-    <div class="tab-content">
-        <!-- Home tab content -->
+    <!-- <div class="tab-content">
         <div class="tab-pane" id="control-sidebar-home-tab">
             <h3 class="control-sidebar-heading">Recent Activity</h3>
             <ul class='control-sidebar-menu'>
@@ -101,7 +100,6 @@ use dmstr\widgets\Alert;
                     </a>
                 </li>
             </ul>
-            <!-- /.control-sidebar-menu -->
 
             <h3 class="control-sidebar-heading">Tasks Progress</h3>
             <ul class='control-sidebar-menu'>
@@ -154,12 +152,10 @@ use dmstr\widgets\Alert;
                     </a>
                 </li>
             </ul>
-            <!-- /.control-sidebar-menu -->
+
 
         </div>
-        <!-- /.tab-pane -->
 
-        <!-- Settings tab content -->
         <div class="tab-pane" id="control-sidebar-settings-tab">
             <form method="post">
                 <h3 class="control-sidebar-heading">General Settings</h3>
@@ -174,7 +170,6 @@ use dmstr\widgets\Alert;
                         Some information about this general settings option
                     </p>
                 </div>
-                <!-- /.form-group -->
 
                 <div class="form-group">
                     <label class="control-sidebar-subheading">
@@ -186,7 +181,6 @@ use dmstr\widgets\Alert;
                         Other sets of options are available
                     </p>
                 </div>
-                <!-- /.form-group -->
 
                 <div class="form-group">
                     <label class="control-sidebar-subheading">
@@ -198,7 +192,6 @@ use dmstr\widgets\Alert;
                         Allow the user to show his name in blog posts
                     </p>
                 </div>
-                <!-- /.form-group -->
 
                 <h3 class="control-sidebar-heading">Chat Settings</h3>
 
@@ -208,7 +201,6 @@ use dmstr\widgets\Alert;
                         <input type="checkbox" class="pull-right" checked/>
                     </label>
                 </div>
-                <!-- /.form-group -->
 
                 <div class="form-group">
                     <label class="control-sidebar-subheading">
@@ -216,7 +208,6 @@ use dmstr\widgets\Alert;
                         <input type="checkbox" class="pull-right"/>
                     </label>
                 </div>
-                <!-- /.form-group -->
 
                 <div class="form-group">
                     <label class="control-sidebar-subheading">
@@ -224,12 +215,10 @@ use dmstr\widgets\Alert;
                         <a href="javascript::;" class="text-red pull-right"><i class="fa fa-trash-o"></i></a>
                     </label>
                 </div>
-                <!-- /.form-group -->
             </form>
         </div>
-        <!-- /.tab-pane -->
-    </div>
-</aside><!-- /.control-sidebar -->
+    </div> -->
+<!-- </aside> --><!-- /.control-sidebar -->
 <!-- Add the sidebar's background. This div must be placed
      immediately after the control sidebar -->
 <div class='control-sidebar-bg'></div>
